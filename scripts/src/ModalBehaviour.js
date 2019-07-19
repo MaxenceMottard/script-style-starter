0e5f00a1266982a786a365dc200aee8650d6e13d
MaxenceMottard <mottard.maxence@gmail.com>
2019-07-19 20:07:55 +0200

    Fix same issue

--- scripts/src/ModalBehaviour.js
+++ scripts/src/ModalBehaviour.js
@@ -3,8 +3,8 @@ import MenuBehaviour from "./MenuBehaviour";
 // @TODO: Add accessibility
 
 class ModalBehaviour extends MenuBehaviour {
-    constructor({ elementSelector, btnSelector: openBtnSelector, closeBtnSelector, accessibility = true, accessibilityBreakpoint = 10000 }) {
-        super({ elementSelector, openBtnSelector, accessibility, accessibilityBreakpoint })
+    constructor({ elementSelector, openBtnSelector, closeBtnSelector, accessibility = true, accessibilityBreakpoint = 10000 }) {
+        super({ elementSelector, btnSelector: openBtnSelector, accessibility, accessibilityBreakpoint })
         this.closeButton = document.querySelector( closeBtnSelector )
 
         super.addEventOnButton( this.closeButton )
