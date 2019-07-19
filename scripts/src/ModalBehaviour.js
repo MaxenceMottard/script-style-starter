acd0ae30355f50c98a6df34a1d21879529f2b99c
MaxenceMottard <mottard.maxence@gmail.com>
2019-07-19 20:00:01 +0200

    Fix openBtnSelector issue for ModalBehaviour

--- scripts/src/ModalBehaviour.js
+++ scripts/src/ModalBehaviour.js
@@ -3,7 +3,7 @@ import MenuBehaviour from "./MenuBehaviour";
 // @TODO: Add accessibility
 
 class ModalBehaviour extends MenuBehaviour {
-    constructor({ elementSelector, openBtnSelector, closeBtnSelector, accessibility = true, accessibilityBreakpoint = 10000 }) {
+    constructor({ elementSelector, btnSelector: openBtnSelector, closeBtnSelector, accessibility = true, accessibilityBreakpoint = 10000 }) {
         super({ elementSelector, openBtnSelector, accessibility, accessibilityBreakpoint })
         this.closeButton = document.querySelector( closeBtnSelector )
 
